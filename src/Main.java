52406a9b3f370dfdf7daa8f79dced7812ad7a877
Giulio Marciante <87944393+undeermind@users.noreply.github.com>
2023-12-05 22:45:18 +0100

    Esercizio Testing5

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+//Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
+//aggiungi un anno
+//sottrai un mese
+//aggiungi 7 giorni
+//Stampa il risultato localizzata per l'Italia
+
+public class Main {
+    public static void main(String[] args) {
+
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        System.out.println(changeDateItaly(date));
+    }
+    public static String changeDateItaly(OffsetDateTime date) {
+        OffsetDateTime newDate;
+
+        newDate = date
+                .plusYears(1)
+                .minusMonths(1)
+                .plusDays(7);
+
+        return newDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy", Locale.ITALY));
+    }
+}
---
+++ src/MainTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    private final Main testing = new Main();
+
+    OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
+    @Test
+    void changeDateItalyTest() {
+        String result = testing.changeDateItaly(date);
+        assertEquals("08-02-2024", result);
+    }
+}
